29c605ffd17941f26defbbd35964486321b2a741
Baroen Sudarman <baroenese@gmail.com>
2025-02-14 20:24:26 +0800

    Ubah title list invoice

--- src/app/(theme)/invoices/list/_internal/main-grid.tsx
+++ src/app/(theme)/invoices/list/_internal/main-grid.tsx
@@ -8,7 +8,7 @@ export function MainGrid() {
     return (
         <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
             <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-                Details
+                My Invoice
             </Typography>
             <Grid container spacing={2} columns={12}>
                 <Grid size={{ xs: 12, lg: 12 }}>
